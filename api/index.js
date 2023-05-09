27680bb9ec6e30fa135193f3e5e3c16933ecb3a9
d9914 <altierd@sunypoly.edu>
2023-05-08 21:42:49 -0400

    working on login

--- api/index.js
+++ api/index.js
@@ -24,12 +24,33 @@ app.get("/test", (req, res) => {
 
 app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  const userDoc = await User.create({
-    name,
-    email,
-    password: bcrypt.hashSync(password, bcryptSalt),
-  });
-  res.json(userDoc);
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+
+    if (passOk) {
+      res.json("pass ok");
+    } else {
+      res.status(422).json("no");
+    }
+  } else {
+    res.json("not found");
+  }
 });
 
 app.listen(4000);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,13 +6,19 @@ export default function RegisterPage() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  function registerUser(ev) {
+  async function registerUser(ev) {
     ev.preventDefault();
-    axios.post("/register", {
-      name,
-      email,
-      password,
-    });
+    try {
+      await axios.post("/register", {
+        name,
+        email,
+        password,
+      });
+
+      alert("Registration Successful");
+    } catch (e) {
+      alert("Registration Failed");
+    }
   }
   return (
     <div className="mt-4 grow flex items-center justify-around">
