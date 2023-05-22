831dac4c12868922c6a1598d8f3a242958b97e0f
d9914 <altierd@sunypoly.edu>
2023-05-21 20:28:11 -0400

    bookings

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,6 +1,7 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { UserContext } from "./UserContext";
+import { useContext } from "react";
+import { UserContext } from "./UserContext.jsx";
+
 export default function Header() {
   const { user } = useContext(UserContext);
   return (
@@ -12,15 +13,15 @@ export default function Header() {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-8 h-8"
+          className="w-8 h-8 -rotate-90"
         >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+            d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
           />
         </svg>
-        <span className="font-bold text-xl">Bookings</span>
+        <span className="font-bold text-xl">airbnb</span>
       </Link>
       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
@@ -47,7 +48,7 @@ export default function Header() {
       </div>
       <Link
         to={user ? "/account" : "/login"}
-        className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4"
+        className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 "
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,53 +1,55 @@
 import { Link, useParams } from "react-router-dom";
-
+import AccountNav from "../AccountNav";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import PlaceImg from "../PlaceImg";
 export default function PlacesPage() {
-  const { action } = useParams();
-
+  const [places, setPlaces] = useState([]);
+  useEffect(() => {
+    axios.get("/user-places").then(({ data }) => {
+      setPlaces(data);
+    });
+  }, []);
   return (
     <div>
-      {action !== "new" && (
-        <div className="text-center">
-          <Link
-            classname=" inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full "
-            to={"/account/places/new"}
+      <AccountNav />
+      <div className="text-center">
+        <Link
+          className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
+          to={"/account/places/new"}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
+            <path
+              fillRule="evenodd"
+              d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
+              clipRule="evenodd"
+            />
+          </svg>
+          Add new place
+        </Link>
+      </div>
+      <div className="mt-4">
+        {places.length > 0 &&
+          places.map((place) => (
+            <Link
+              to={"/account/places/" + place._id}
+              className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-            Add new Place
-          </Link>
-        </div>
-      )}
-      {action === "new" && (
-        <div>
-          <form>
-            <h2 className="text-2xl mt-4">Title</h2>
-            <p className="text-gray-500 text-sm">Title for accommodation</p>
-            <input type="text" placeholder="title" />
-            <h2 className="text-2xl mt-4">Address</h2>
-            <p className="text-gray-500 text-sm">Adress</p>
-            <input type="text" placeholder="address" />
-            <h2 className="text-2xl mt-4">Photos</h2>v
-            <p className="text-gray-500 text-sm">More photos=better</p>
-            <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 ">
-              <button className="border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
-                +
-              </button>
-            </div>
-          </form>
-        </div>
-      )}
+              <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
+                <PlaceImg place={place} />
+              </div>
+              <div className="grow-0 shrink">
+                <h2 className="text-xl">{place.title}</h2>
+                <p className="text-sm mt-2">{place.description}</p>
+              </div>
+            </Link>
+          ))}
+      </div>
     </div>
   );
 }
